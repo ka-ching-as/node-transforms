03afe4ac31198e7742481c2d03db2a1bbcc24eb4
Bo Gosmer <beegee@tutanota.com>
2019-06-05 10:26:25 +0200

    Exposing recommendation transformations

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,3 +4,6 @@ export * from "./ProductTransformation";
 export * from "./StockTransformation";
 export * from "./ShopifyStockTransformation";
 export * from "./KachingStockTransformation";
+export * from "./RecommendationTransformation";
+export * from "./KachingRecommendationTransformation";
+export * from "./RaptorRecommendationTransformation";
