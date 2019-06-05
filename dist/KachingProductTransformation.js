286ec4eda365dda7aa466ca4b0e58bb4d6b2d457
Bo Gosmer <beegee@tutanota.com>
2019-06-05 11:25:54 +0200

    Using es2015 as target. Unit tests in functions won't run now so I'm trying things out

--- dist/KachingProductTransformation.js
+++ dist/KachingProductTransformation.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 class KachingProductTransformation {
     // We do not (yet) support deletion through the native Ka-ching format 
@@ -20,37 +28,39 @@ class KachingProductTransformation {
         }
         return [];
     }
-    async transformRepoProduct(input, defaultChannels, defaultMarkets, callback) {
-        if (input.product) {
-            await callback(input);
-        }
-        else if (input.products) {
-            for (const key in input.products) {
-                const product = input.products[key];
-                const obj = { product: product };
-                if (input.metadata) {
-                    obj["metadata"] = input.metadata;
-                }
-                else if (defaultChannels.length > 0 || defaultMarkets.length > 0) {
-                    const channels = {};
-                    const markets = {};
-                    for (const channel of defaultChannels) {
-                        channels[channel] = true;
-                    }
-                    for (const market of defaultMarkets) {
-                        markets[market] = true;
-                    }
-                    obj["metadata"] = { channels: channels, markets: markets };
-                }
-                if (input.shops) {
-                    obj["shops"] = input.shops;
-                }
-                await callback(obj);
+    transformRepoProduct(input, defaultChannels, defaultMarkets, callback) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (input.product) {
+                yield callback(input);
             }
-        }
-        else {
-            throw new Error("Bad request - missing product or products key in body.");
-        }
+            else if (input.products) {
+                for (const key in input.products) {
+                    const product = input.products[key];
+                    const obj = { product: product };
+                    if (input.metadata) {
+                        obj["metadata"] = input.metadata;
+                    }
+                    else if (defaultChannels.length > 0 || defaultMarkets.length > 0) {
+                        const channels = {};
+                        const markets = {};
+                        for (const channel of defaultChannels) {
+                            channels[channel] = true;
+                        }
+                        for (const market of defaultMarkets) {
+                            markets[market] = true;
+                        }
+                        obj["metadata"] = { channels: channels, markets: markets };
+                    }
+                    if (input.shops) {
+                        obj["shops"] = input.shops;
+                    }
+                    yield callback(obj);
+                }
+            }
+            else {
+                throw new Error("Bad request - missing product or products key in body.");
+            }
+        });
     }
 }
 exports.KachingProductTransformation = KachingProductTransformation;
